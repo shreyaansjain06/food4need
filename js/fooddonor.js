79e502b119f8e16e23a05b40d5dd45f35f4c880b
shreyaansjain <shreyaansjain60@gmail.com>
2021-10-20 15:34:05 +0530

    modify fooddonor.js

--- js/fooddonor.js
+++ js/fooddonor.js
@@ -1,55 +1,116 @@
+import {
+  getDatabase,
+  ref,
+  set,
+  update,
+  remove,
+  child,
+  get,
+} from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js';
+const db = getDatabase();
+var name,
+  email,
+  address,
+  city,
+  state,
+  zip,
+  country,
+  telephone,
+  product,
+  quantity;
 
-
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js"
-var firebaseConfig = {
-    apiKey: "AIzaSyD4YqaLrYlfXz_gptVe7rGlTGRXEzpFBA4",
-    authDomain: "food4need-4f9c0.firebaseapp.com",
-    databaseURL: "https://food4need-4f9c0-default-rtdb.firebaseio.com",
-    projectId: "food4need-4f9c0",
-    storageBucket: "food4need-4f9c0.appspot.com",
-    messagingSenderId: "657260809429",
-    appId: "1:657260809429:web:5c24f9d057a6efea93a65c"
-  };
-
-
-const app=initializeApp(firebaseConfig);
-import { getDatabase, ref, set,update,remove} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
-const db=getDatabase()
-var name,email,address,city,state,zip,country,telephone,product,quantity;
-
-function readForm(){
-  name=document.getElementById("name").value;
-  email=document.getElementById("email").value;
-  address=document.getElementById("address").value;
-  city=document.getElementById("city").value;
-  state=document.getElementById("state").value;
-  zip=document.getElementById("zip").value;
-  country=document.getElementById("country").value;
-  telephone=document.getElementById("telephone").value;
-  product=document.getElementById("product").value;
-  quantity=document.getElementById("quantity").value;
- console.log(name,email,address,city,state,zip,country,telephone,product,quantity);
+function readForm() {
+  name = document.getElementById('name').value;
+  email = document.getElementById('email').value;
+  address = document.getElementById('address').value;
+  city = document.getElementById('city').value;
+  state = document.getElementById('state').value;
+  zip = document.getElementById('zip').value;
+  country = document.getElementById('country').value;
+  telephone = document.getElementById('telephone').value;
+  product = document.getElementById('product').value;
+  quantity = document.getElementById('quantity').value;
+  console.log(
+    name,
+    email,
+    address,
+    city,
+    state,
+    zip,
+    country,
+    telephone,
+    product,
+    quantity
+  );
 }
-document.getElementById('submit').onclick=function(e){
+document.getElementById('submit').onclick = function (e) {
   e.preventDefault();
   readForm();
-set(ref(db,"food/"+ name),{
-  name:name,
-  email:email,
-  address:address,
-  city:city,
-  state:state,
-  zip:zip,
-  country:country,
-  telphone:telephone,
-  product:product,
-  quantity:quantity
-})
-.then(()=>{
-  alert("successfully stored data")
-})
-.catch((e)=>{
-  alert(e, "unsuccesfully")
-})
+  set(ref(db, 'food/' + telephone), {
+    name: name,
+    email: email,
+    address: address,
+    city: city,
+    state: state,
+    zip: zip,
+    country: country,
+    telephone: telephone,
+    product: product,
+    quantity: quantity,
+  })
+    .then(() => {
+      alert('successfully stored data');
+    })
+    .catch((e) => {
+      alert(e, 'unsuccesfully');
+    });
+};
 
-}
+const getData = () => {
+  let remaining, fooditem;
+  const dbref = ref(getDatabase());
+  get(child(dbref, `food/${telephone}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        remaining = snapshot.val().quantity;
+        fooditem = snapshot.val().product;
+        console.log(remaining, fooditem, telephone);
+        console.log(snapshot.val());
+        //
+        if (fooditem === product) {
+          if (remaining > 1) {
+            update(ref(db, 'food/' + telephone), {
+              quantity: remaining - 1,
+            })
+              .then(() => {
+                alert('successfully reduced');
+              })
+              .catch((e) => {
+                alert(e, 'error');
+              });
+          } else {
+            remove(ref(db, 'food/' + telephone), {})
+              .then(() => {
+                alert('successfully deleted');
+              })
+              .catch((error) => {
+                alert(error);
+              });
+          }
+        } else {
+          alert('product does not exist');
+        }
+      } else {
+        console.log('No data available');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+document.getElementById('reduce').onclick = function (e) {
+  e.preventDefault();
+  readForm();
+  getData();
+};
